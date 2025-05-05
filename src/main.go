44866209b1532a7b330c626ba02ed5cf3af20854
Bedil Lieurr <154463051+bedillieurr@users.noreply.github.com>
2025-05-05 12:49:12 +0700

    main.go

--- src/main.go
+++ src/main.go
@@ -1,1 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "log"
+)
+
+func main() {
+    fmt.Println("Starting Nacara Scanner...")
+
+    url := "http://example.com" // Ganti dengan target yang sesuai
+    autoTemplateDiscovery(url)
+    wafBypass(url)
+    apiVulnerabilityScan(url)
+    headlessBrowserScan(url)
+
+    fmt.Println("\nScan Complete.")
+    fmt.Println("nacara v1 by bedillieurr")
+}
 
