05238c8f707ebf8e7a3ae0287c7c1898dd78337f
Bedil Lieurr <154463051+bedillieurr@users.noreply.github.com>
2025-05-05 12:50:01 +0700

    cloud.go

--- src/cloud.go
+++ src/cloud.go
@@ -1,1 +1,12 @@
+package main
+
+import (
+    "fmt"
+)
+
+// Cloud resource scan (misalnya, S3 buckets)
+func scanCloudResources(url string) {
+    fmt.Printf("Scanning cloud resource for vulnerabilities: %s\n", url)
+    // Logika untuk memeriksa S3 bucket terbuka atau Cloud Function yang tidak terautentikasi
+}
 
